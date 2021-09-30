eec190b62bfb27f0ccf9bbfecdb1b6f31122500b
Matias Villegas <mati.villegas23@gmail.com>
2021-09-30 20:51:03 -0300

    add test done

--- model/entities_test.go
+++ model/entities_test.go
@@ -1,29 +1,27 @@
-package main
+package model
 
 import (
 	"testing"
-	"entregablego.com/model"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestType(t *testing.T) {
 	s := "TX04ACBD"
 	type_ := "TX"
-	actual, _ := model.GetStructure(s)
+	actual, _ := GetStructure(s)
 	assert.Equal(t, type_, actual.Type, "Las cadenas de texto resultantes son diferentes")
 }
 
 func TestLength(t *testing.T) {
 	s := "TX04ACBD"
 	length := 4
-	actual, _ := model.GetStructure(s)
+	actual, _ := GetStructure(s)
 	assert.Equal(t, length, actual.Length, "La/s cadena/s de numero/s resultantes son diferentes")
 }
 
 func TestValue(t *testing.T) {
 	s := "TX04ACBD"
 	value := "ACBD"
-	actual, _ := model.GetStructure(s)
+	actual, _ := GetStructure(s)
 	assert.Equal(t, value, actual.Value, "Las cadenas de texto resultantes son diferentes")
 }
-
